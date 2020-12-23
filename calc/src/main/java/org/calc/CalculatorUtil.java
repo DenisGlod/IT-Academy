a40fe8aff614fc5172631ba04c6175e69d8001eb
Denis GLod <denis.glod@mail.ru>
2020-12-23 12:54:16 +0300

    Updated classes App, CalculatorUtil, Employee and .gitignore file.

--- calc/src/main/java/org/calc/CalculatorUtil.java
+++ calc/src/main/java/org/calc/CalculatorUtil.java
@@ -1,20 +1,23 @@
 package org.calc;
 
-import java.util.TreeMap;
+import java.util.Map;
 
 public class CalculatorUtil {
 
-    public static Double averageSalary(TreeMap<Integer, Double> salary, Integer startMonth, Integer endMonth)
-	    throws Exception {
-	if (startMonth < 1 || startMonth > 12 || endMonth < 1 || endMonth > 12 || startMonth >= endMonth) {
-	    throw new Exception("Incorrect period");
-	}
-	double sum = 0;
-	int countMonth = endMonth - startMonth + 1;
-	for (int i = startMonth; i <= endMonth; i++) {
-	    sum += salary.get(i);
-	}
-	return sum / countMonth;
+    public static Double averageSalary(Map<Integer, Double> salary, Integer startMonth, Integer endMonth) {
+        isValidMonth(startMonth, endMonth);
+        double sum = 0;
+        int countMonth = endMonth - startMonth + 1;
+        for (int i = startMonth; i <= endMonth; i++) {
+            sum += salary.get(i);
+        }
+        return sum / countMonth;
+    }
+
+    private static void isValidMonth(Integer startMonth, Integer endMonth) {
+        if (startMonth < 1 || startMonth > 12 || endMonth < 1 || endMonth > 12 || startMonth >= endMonth) {
+            throw new IllegalArgumentException("Incorrect period");
+        }
     }
 
 }
