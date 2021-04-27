89ba440fab0ceacccf744818a0dcbf450fcb8dfe
Denis Glod <denis.glod@mail.ru>
2021-04-27 12:59:14 +0300

    Updated pom.xml in all modules Updated UserRepository

--- dao/src/main/java/by/freebook/dao/repository/UserRepository.java
+++ dao/src/main/java/by/freebook/dao/repository/UserRepository.java
@@ -1,21 +1,15 @@
 package by.freebook.dao.repository;
 
 import by.freebook.dao.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.lang.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmailAndPassword(String email, String password);
 
     Optional<User> findUserById(Long id);
 
     Optional<User> deleteUserById(Long id);
-
-    @NonNull
-    @Override
-    List<User> findAll();
 
 }
