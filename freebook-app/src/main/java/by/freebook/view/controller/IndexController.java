5b25683a4f5c1abe8fa372bf80bf612ec6a0e246
Denis Glod <denis.glod@mail.ru>
2021-05-02 01:36:28 +0300

    Updated index.html and login.html

--- freebook-app/src/main/java/by/freebook/view/controller/IndexController.java
+++ freebook-app/src/main/java/by/freebook/view/controller/IndexController.java
@@ -1,17 +1,20 @@
 package by.freebook.view.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Slf4j
 @Controller
 public class IndexController {
 
     @RequestMapping(path = {"/", "/index.html"}, method = RequestMethod.GET)
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(path = {"/login", ""}, method = RequestMethod.GET)
+    public String login() {
+        return "page/login";
     }
 
 }
